026bdd53e2dba1f40bc60258df4914ac0fc51d85
wisdomkhan <51525305+wisdomkhan@users.noreply.github.com>
2021-01-15 20:11:45 +0530

    Revert "Add files via upload"

--- 2.c
+++
@@ -1,9 +0,0 @@
-# include <stdio.h>
-int main(){
-  float a, r;
-  printf("Enter radius\n");
-  scanf("%f", &r);
-  a = 3.14 * r * r;
-  printf("%f", a);
-  return 0;
-}
--- sep2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-  double x = 32.456;
-  int i = x;
-  double f = x - i;
-  printf("%d\n", i);
-  printf("%lf\n", f);
-}
